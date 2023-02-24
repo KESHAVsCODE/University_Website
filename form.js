10458c3e6ff687e0c01fe7b6944d342240232e64
KESHAVsCODE <106483384+KESHAVsCODE@users.noreply.github.com>
2023-02-24 17:24:39 +0530

    Update form.js

--- form.js
+++ form.js
@@ -1,5 +1,5 @@
   const submitBtn = document.getElementById('submit-btn');
   submitBtn.addEventListener('click', function(event) {
-    event.preventDefault(); // prevent the form from submitting normally
-    alert('Your message has been sent successfully!'); // show pop-up message
+    event.preventDefault(); 
+    alert('Your message has been sent successfully!');
   });
