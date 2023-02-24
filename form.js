789648b6d804490a6b5dd76de5ee03552ff8e967
KESHAVsCODE <106483384+KESHAVsCODE@users.noreply.github.com>
2023-02-24 18:21:32 +0530

    Update form.js

--- form.js
+++ form.js
@@ -1,5 +1,9 @@
   const submitBtn = document.querySelector(".submit-btn");
+  const myForm = document.querySelector(".comment-form");
   submitBtn.addEventListener('click', function(event) {
-    event.preventDefault(); 
+    event.preventDefault();
+    
     alert('Your message has been sent successfully!');
+    
+    myForm.reset();
   });
